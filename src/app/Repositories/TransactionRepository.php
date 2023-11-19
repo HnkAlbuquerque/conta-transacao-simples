ef43ad39824fe7071983bf4b57e0e8cea81329f7
Henrique Albuquerque <henrique-albuquerque@hotmail.com>
2023-11-19 16:15:49 -0300

    feat(objective-test): Correção do retorno da conta e saldo após realizar o update

--- src/app/Repositories/TransactionRepository.php
+++ src/app/Repositories/TransactionRepository.php
@@ -12,7 +12,7 @@ class TransactionRepository
         try {
             return DB::transaction(function () use($account, $sum) {
                 Account::where('conta_id', $account->conta_id)->update(['saldo' => $sum]);
-                return Account::find($account->conta_id)->first();
+                return Account::where('conta_id', $account->conta_id)->first();
             });
         } catch (\Exception $exception) {
             return response()->json(['errors' => ['message' => $exception->getMessage()]], $exception->getCode());
